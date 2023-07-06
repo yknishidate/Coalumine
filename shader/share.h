15e2743c63aa7f0eea9db7c38d27a2e63f058081
nishidate-yuki <yuki18425@gmail.com>
2023-07-06 15:18:38 +0900

    add noise

--- shader/share.h
+++ shader/share.h
@@ -4,11 +4,13 @@ struct PushConstants {
     glm::mat4 invView;
     glm::mat4 invProj;
     int frame = 0;
+    float noiseFreq = 1.0;
 };
 #else
 layout(push_constant) uniform PushConstants {
     mat4 invView;
     mat4 invProj;
     int frame;
+    float noiseFreq;
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -109,7 +109,7 @@ public:
     }
 
     void onRender(const CommandBuffer& commandBuffer) override {
-        ImGui::SliderInt("Test slider", &testInt, 0, 100);
+        ImGui::SliderFloat("Noise frequency", &pushConstants.noiseFreq, 0.01, 100.0);
         commandBuffer.bindDescriptorSet(descSet, pipeline);
         commandBuffer.bindPipeline(pipeline);
         commandBuffer.pushConstants(pipeline, &pushConstants);
@@ -122,7 +122,6 @@ public:
     DescriptorSet descSet;
     ComputePipeline pipeline;
     OrbitalCamera camera;
-    int testInt = 0;
     PushConstants pushConstants;
 };
 
