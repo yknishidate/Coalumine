14168da8505cef7e015cc06a0ad369f570da0168
nishidate-yuki <yuki18425@gmail.com>
2023-07-16 23:26:22 +0900

    use rt pipeline

--- shader/share.h
+++ shader/share.h
@@ -1,58 +1,28 @@
 
 #ifdef __cplusplus
 struct PushConstants {
-    // 128
+    // Camera
     glm::mat4 invView;
     glm::mat4 invProj;
 
-    // 32
-    float absorptionIntensity = 1.0f;
-    float emissionIntensity = 1.0f;
-    int enableToneMapping = 0;
-    int enableGammaCorrection = 0;
-
-    // 32
     int frame = 0;
-    int enableNoise = 1;
+
+    // Bloom
     int enableBloom = 0;
-    int enableFlowNoise = 0;
-
-    // 32
-    float bloomIntensity = 10.0f;
-    float bloomThreshold = 0.4f;
-    float lightIntensity = 1.0f;
-    float flowSpeed = 0.025f;
-
-    // 32
-    float absorption[4] = {0.9f, 0.9f, 0.9f, 0.9f};
-
-    float volumeSize[4];
-    int blurSize = 32;
-    float scatterIntensity = 1.0;
+    int blurSize = 16;
+    float bloomIntensity = 1.0f;
+    float bloomThreshold = 0.5f;
 };
 #else
 layout(push_constant) uniform PushConstants {
     mat4 invView;
     mat4 invProj;
 
-    float absorptionIntensity;
-    float emissionIntensity;
-    int enableToneMapping;
-    int enableGammaCorrection;
-
     int frame;
-    int enableNoise;
-    int enableBloom;
-    int enableFlowNoise;
 
+    int enableBloom;
+    int blurSize;
     float bloomIntensity;
     float bloomThreshold;
-    float lightIntensity;
-    float flowSpeed;
-
-    vec4 absorption;
-    vec4 volumeSize;
-    int blurSize;
-    float scatterIntensity;
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,10 @@
 ﻿#include "../shader/share.h"
 #include "App.hpp"
 
-#define NOMINMAX
 #include <Windows.h>
 #undef near
 #undef far
 #undef RGB
-
-#define IMATH_HALF_NO_LOOKUP_TABLE
-#include <openvdb/openvdb.h>
-
-using FloatGrid = openvdb::FloatGrid;
-using Vec3SGrid = openvdb::Vec3SGrid;
 
 namespace fs = std::filesystem;
 fs::path getExecutableDirectory() {
@@ -50,16 +43,6 @@ bool shouldRecompile(const std::string& shaderFileName, const std::string& entry
     return !fs::exists(spvFile) || glslWriteTime > fs::last_write_time(spvFile);
 }
 
-bool shouldRecompile(const std::string& shaderFileName,
-                     const std::vector<std::string>& entryPoints) {
-    for (auto& entryPoint : entryPoints) {
-        if (shouldRecompile(shaderFileName, entryPoint)) {
-            return true;
-        }
-    }
-    return false;
-}
-
 std::vector<uint32_t> compileShader(const std::string& shaderFileName,
                                     const std::string& entryPoint) {
     auto glslFile = getShaderSourceDirectory() / shaderFileName;
@@ -85,53 +68,67 @@ public:
         : App({
               .width = 1920,
               .height = 1080,
-              .title = "HelloCompute",
+              .title = "rtcamp9",
               .enableValidation = true,
           }) {}
 
     void createPipelines() {
-        std::vector<Shader> shaders(entryPoints.size());
-        std::vector<const Shader*> shaderPtrs(entryPoints.size());
-        for (int i = 0; i < entryPoints.size(); i++) {
-            std::vector<uint32_t> code;
-            if (shouldRecompile(shaderFileName, entryPoints[i])) {
-                code = compileShader(shaderFileName, entryPoints[i]);
-            } else {
-                code = readShader(shaderFileName, entryPoints[i]);
-            }
-            shaders[i] = context.createShader({
-                .code = code,
-                .stage = vk::ShaderStageFlagBits::eCompute,
-            });
-            shaderPtrs[i] = &shaders[i];
-        }
+        std::vector<Shader> shaders(5);
+        shaders[0] = context.createShader({
+            .code = compileShader("blur.comp", "main"),
+            .stage = vk::ShaderStageFlagBits::eCompute,
+        });
+        shaders[1] = context.createShader({
+            .code = compileShader("composite.comp", "main"),
+            .stage = vk::ShaderStageFlagBits::eCompute,
+        });
+        shaders[2] = context.createShader({
+            .code = compileShader("base.rgen", "main"),
+            .stage = vk::ShaderStageFlagBits::eRaygenKHR,
+        });
+        shaders[3] = context.createShader({
+            .code = compileShader("base.rmiss", "main"),
+            .stage = vk::ShaderStageFlagBits::eMissKHR,
+        });
+        shaders[4] = context.createShader({
+            .code = compileShader("base.rchit", "main"),
+            .stage = vk::ShaderStageFlagBits::eClosestHitKHR,
+        });
 
         descSet = context.createDescriptorSet({
-            .shaders = shaderPtrs,
+            .shaders = {&shaders[0], &shaders[1]},
             .images =
                 {
                     {"baseImage", baseImage},
-                    {"volumeImage", volumeImage},
                     {"bloomImage", bloomImage},
                     {"finalImage", finalImage},
                 },
         });
 
-        for (int i = 0; i < entryPoints.size(); i++) {
-            pipelines[entryPoints[i]] = context.createComputePipeline({
-                .computeShader = shaders[i],
-                .descSetLayout = descSet.getLayout(),
-                .pushSize = sizeof(PushConstants),
-            });
-        }
+        computePipelines["blur"] = context.createComputePipeline({
+            .computeShader = shaders[0],
+            .descSetLayout = descSet.getLayout(),
+            .pushSize = sizeof(PushConstants),
+        });
+        computePipelines["composite"] = context.createComputePipeline({
+            .computeShader = shaders[1],
+            .descSetLayout = descSet.getLayout(),
+            .pushSize = sizeof(PushConstants),
+        });
+        rayTracingPipeline = context.createRayTracingPipeline({
+            .rgenShader = shaders[2],
+            .missShader = shaders[3],
+            .chitShader = shaders[4],
+            .descSetLayout = descSet.getLayout(),
+            .pushSize = sizeof(PushConstants),
+            .maxRayRecursionDepth = 2,
+        });
     }
 
     void onStart() override {
         spdlog::info("Executable directory: {}", getExecutableDirectory().string());
         spdlog::info("Shader source directory: {}", getShaderSourceDirectory().string());
         spdlog::info("SPIR-V directory: {}", getSpvDirectory().string());
-
-        loadVDB();
 
         baseImage = context.createImage({
             .usage = vk::ImageUsageFlagBits::eStorage | vk::ImageUsageFlagBits::eTransferDst |
@@ -176,23 +173,23 @@ public:
         pushConstants.invProj = camera.getInvProj();
     }
 
+    void recreatePipelinesIfShadersWereUpdated() {
+        bool shouldRecreate = false;
+        shouldRecreate |= shouldRecompile("blur.main", "main");
+        shouldRecreate |= shouldRecompile("composite.main", "main");
+        if (shouldRecreate) {
+            try {
+                createPipelines();
+            } catch (const std::exception& e) {
+                spdlog::error(e.what());
+            }
+        }
+    }
+
     void onRender(const CommandBuffer& commandBuffer) override {
         static int imageIndex = 0;
         static int blurIteration = 32;
         ImGui::Combo("Image", &imageIndex, "Render\0Bloom");
-        ImGui::Checkbox("Enable noise", reinterpret_cast<bool*>(&pushConstants.enableNoise));
-        ImGui::ColorPicker4("Absorption color", pushConstants.absorption);
-        ImGui::SliderFloat("Absorption intensity", &pushConstants.absorptionIntensity, 0.0, 1.0);
-        ImGui::SliderFloat("Scattering intensity", &pushConstants.scatterIntensity, 0.0, 1.0);
-        ImGui::SliderFloat("Emission intensity", &pushConstants.emissionIntensity, 0.0, 10.0);
-
-        ImGui::SliderFloat("Light intensity", &pushConstants.lightIntensity, 0.0, 10.0);
-
-        // Post effects
-        ImGui::Checkbox("Enable tone mapping",
-                        reinterpret_cast<bool*>(&pushConstants.enableToneMapping));
-        ImGui::Checkbox("Enable gamma correction",
-                        reinterpret_cast<bool*>(&pushConstants.enableGammaCorrection));
 
         // Bloom
         ImGui::Checkbox("Enable bloom", reinterpret_cast<bool*>(&pushConstants.enableBloom));
@@ -203,37 +200,25 @@ public:
             ImGui::SliderInt("Blur size", &pushConstants.blurSize, 0, 64);
         }
 
-        // Flow noise
-        ImGui::Checkbox("Enable flow noise",
-                        reinterpret_cast<bool*>(&pushConstants.enableFlowNoise));
-        if (pushConstants.enableFlowNoise) {
-            ImGui::SliderFloat("Flow speed", &pushConstants.flowSpeed, 0.0, 0.2);
-        }
-
+        // Show GPU time
         if (pushConstants.frame > 1) {
             ImGui::Text("GPU time: %f ms", gpuTimer.elapsedInMilli());
         }
-        if (shouldRecompile(shaderFileName, entryPoints)) {
-            try {
-                createPipelines();
-            } catch (const std::exception& e) {
-                spdlog::error(e.what());
-            }
-        }
 
-        commandBuffer.bindDescriptorSet(descSet, pipelines["main_base"]);
+        // Check shader files
+        recreatePipelinesIfShadersWereUpdated();
 
         commandBuffer.beginTimestamp(gpuTimer);
 
         // Base rendering
-        commandBuffer.bindPipeline(pipelines["main_base"]);
-        commandBuffer.pushConstants(pipelines["main_base"], &pushConstants);
-        commandBuffer.dispatch(pipelines["main_base"], width / 8, height / 8, 1);
+
+        ///
+        /// Render base image here
+        ///
 
         commandBuffer.imageBarrier(
             vk::PipelineStageFlagBits::eComputeShader, vk::PipelineStageFlagBits::eComputeShader,
             {}, baseImage, vk::AccessFlagBits::eShaderWrite, vk::AccessFlagBits::eShaderRead);
-
         commandBuffer.imageBarrier(
             vk::PipelineStageFlagBits::eComputeShader, vk::PipelineStageFlagBits::eComputeShader,
             {}, bloomImage, vk::AccessFlagBits::eShaderWrite, vk::AccessFlagBits::eShaderRead);
@@ -241,9 +226,9 @@ public:
         // Blur
         if (pushConstants.enableBloom) {
             for (int i = 0; i < blurIteration; i++) {
-                commandBuffer.bindPipeline(pipelines["main_blur"]);
-                commandBuffer.pushConstants(pipelines["main_blur"], &pushConstants);
-                commandBuffer.dispatch(pipelines["main_blur"], width / 8, height / 8, 1);
+                commandBuffer.bindPipeline(computePipelines["blur"]);
+                commandBuffer.pushConstants(computePipelines["blur"], &pushConstants);
+                commandBuffer.dispatch(computePipelines["blur"], width / 8, height / 8, 1);
 
                 commandBuffer.imageBarrier(vk::PipelineStageFlagBits::eComputeShader,
                                            vk::PipelineStageFlagBits::eComputeShader, {},
@@ -253,9 +238,9 @@ public:
         }
 
         // Composite
-        commandBuffer.bindPipeline(pipelines["main_composite"]);
-        commandBuffer.pushConstants(pipelines["main_composite"], &pushConstants);
-        commandBuffer.dispatch(pipelines["main_composite"], width / 8, height / 8, 1);
+        commandBuffer.bindPipeline(computePipelines["composite"]);
+        commandBuffer.pushConstants(computePipelines["composite"], &pushConstants);
+        commandBuffer.dispatch(computePipelines["composite"], width / 8, height / 8, 1);
 
         commandBuffer.endTimestamp(gpuTimer);
 
@@ -264,105 +249,14 @@ public:
                                 height);
     }
 
-    void loadVDB() {
-        openvdb::initialize();
-
-        openvdb::io::File file("../asset/nebula_doxia.filecache1_v18.0001.vdb");
-        file.open();
-
-        openvdb::GridBase::Ptr grid = file.readGrid("emission");
-        file.close();
-
-        // Cast the grid to a FloatGrid pointer
-        FloatGrid::Ptr floatGrid = openvdb::gridPtrCast<FloatGrid>(grid);
-
-        // Create a 3D vector to hold the data
-        openvdb::CoordBBox bbox = grid->evalActiveVoxelBoundingBox();
-        std::cout << "Min: " << bbox.min() << "\n";
-        std::cout << "Max: " << bbox.max() << "\n";
-
-        openvdb::Coord volumeAreaSize = bbox.max() - bbox.min();
-        uint32_t voxelCount = volumeAreaSize.x() * volumeAreaSize.y() * volumeAreaSize.z();
-        std::cout << "Voxel count: " << voxelCount << std::endl;
-
-        pushConstants.volumeSize[0] = volumeAreaSize.x() * 0.01f;
-        pushConstants.volumeSize[1] = volumeAreaSize.y() * 0.01f;
-        pushConstants.volumeSize[2] = volumeAreaSize.z() * 0.01f;
-        pushConstants.volumeSize[3] = 1.0f;
-
-        std::vector<float> gridData(voxelCount);
-        CPUTimer timer;
-        for (int32_t z = 0; z < volumeAreaSize.z(); ++z) {
-            for (int32_t y = 0; y < volumeAreaSize.y(); ++y) {
-                for (int32_t x = 0; x < volumeAreaSize.x(); ++x) {
-                    openvdb::Coord xyz(x, y, z);
-                    float value = floatGrid->getAccessor().getValue(bbox.min() + xyz);
-                    int32_t index =
-                        x + volumeAreaSize.x() * y + (volumeAreaSize.x() * volumeAreaSize.y() * z);
-                    gridData[index] = value;
-                }
-            }
-            std::cout << z << "/" << volumeAreaSize.z() << std::endl;
-        }
-        std::cout << "VDB loaded: " << timer.elapsedInMilli() << "ms" << std::endl;
-
-        uint32_t byteSize = voxelCount * sizeof(float);
-        std::cout << "Byte size: " << byteSize << std::endl;
-
-        HostBuffer stagingBuffer = context.createHostBuffer({
-            .usage = BufferUsage::Staging,
-            .size = byteSize,
-            .data = gridData.data(),
-        });
-        std::cout << "Staging buffer created" << std::endl;
-
-        volumeImage = context.createImage({
-            .usage = vk::ImageUsageFlagBits::eStorage | vk::ImageUsageFlagBits::eTransferDst |
-                     vk::ImageUsageFlagBits::eTransferSrc | vk::ImageUsageFlagBits::eSampled,
-            .initialLayout = vk::ImageLayout::eTransferDstOptimal,
-            .aspect = vk::ImageAspectFlagBits::eColor,
-            .width = static_cast<uint32_t>(volumeAreaSize.x()),
-            .height = static_cast<uint32_t>(volumeAreaSize.y()),
-            .depth = static_cast<uint32_t>(volumeAreaSize.z()),
-            .format = vk::Format::eR32Sfloat,
-        });
-        std::cout << "Texture created" << std::endl;
-
-        vk::ImageSubresourceLayers subresourceLayers;
-        subresourceLayers.setAspectMask(vk::ImageAspectFlagBits::eColor);
-        subresourceLayers.setMipLevel(0);
-        subresourceLayers.setBaseArrayLayer(0);
-        subresourceLayers.setLayerCount(1);
-
-        vk::Extent3D extent;
-        extent.setWidth(static_cast<uint32_t>(volumeAreaSize.x()));
-        extent.setHeight(static_cast<uint32_t>(volumeAreaSize.y()));
-        extent.setDepth(static_cast<uint32_t>(volumeAreaSize.z()));
-
-        vk::BufferImageCopy region;
-        region.imageSubresource = subresourceLayers;
-        region.imageExtent = extent;
-
-        context.oneTimeSubmit([&](vk::CommandBuffer commandBuffer) {
-            // Image::setImageLayout(commandBuffer, volumeImage.getImage(),
-            //                       vk::ImageLayout::eTransferDstOptimal);
-            commandBuffer.copyBufferToImage(stagingBuffer.getBuffer(), volumeImage.getImage(),
-                                            vk::ImageLayout::eTransferDstOptimal, region);
-            Image::setImageLayout(commandBuffer, volumeImage.getImage(), vk::ImageLayout::eGeneral);
-        });
-        std::cout << "Texture filled" << std::endl;
-    }
-
-    std::string shaderFileName = "render.comp";
-    std::vector<std::string> entryPoints = {"main_base", "main_blur", "main_composite"};
-
     Image baseImage;
     Image volumeImage;
     Image bloomImage;
     Image finalImage;
 
     DescriptorSet descSet;
-    std::unordered_map<std::string, ComputePipeline> pipelines;
+    RayTracingPipeline rayTracingPipeline;
+    std::unordered_map<std::string, ComputePipeline> computePipelines;
     OrbitalCamera camera;
     PushConstants pushConstants;
     GPUTimer gpuTimer;
