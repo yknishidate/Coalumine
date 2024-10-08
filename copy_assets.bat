4590f7fdc63793674286fb989294d816179266f0
Yuki Nishidate <asta18425@gmail.com>
2024-10-08 22:40:17 +0900

    update copy script

--- copy_assets.bat
+++ copy_assets.bat
@@ -1,2 +1,12 @@
-xcopy /e /y .\asset .\build\vs\Debug\asset\
-xcopy /e /y .\asset .\build\vs\Release\asset\
+@echo off
+set SRC=.\asset
+set DST_DEBUG=.\build\vs\Debug\asset
+set DST_RELEASE=.\build\vs\Release\asset
+
+echo Copying files to Debug folder...
+robocopy "%SRC%" "%DST_DEBUG%" /e /xo
+
+echo Copying files to Release folder...
+robocopy "%SRC%" "%DST_RELEASE%" /e /xo
+
+echo Copy operation complete.
